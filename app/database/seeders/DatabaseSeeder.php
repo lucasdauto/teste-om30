42c9cdc963fac9385e9e817e627e8574539e39a6
Lucas Dauto <lucasdauto@gmail.com>
2023-03-23 05:28:53 -0300

    config seeds

--- app/database/seeders/DatabaseSeeder.php
+++ app/database/seeders/DatabaseSeeder.php
@@ -3,7 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Paciente;
+use Database\Factories\PacienteFactory;
 use Illuminate\Database\Seeder;
+use Mockery\Generator\StringManipulation\Pass\Pass;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +22,9 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $this->call([
+            PacienteSeeder::class,
+        ]);
     }
 }
