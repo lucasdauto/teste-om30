e1702403ce2494e1eca7bba21ceacf7ab4670c15
Lucas Dauto <lucasdauto@gmail.com>
2023-03-23 22:36:28 -0300

    api

--- app/routes/api.php
+++ app/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PacienteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('paciente', PacienteController::class);
+Route::get('paciente/search',[PacienteController::class,'search'])->name('paciente.search');
