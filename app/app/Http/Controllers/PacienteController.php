7ac57fddc66011f8306e59bb0dd9390bac70aa1f
Lucas Dauto <lucasdauto@gmail.com>
2023-03-24 02:43:02 -0300

    requests com injeção de dependencia

--- app/app/Http/Controllers/PacienteController.php
+++ app/app/Http/Controllers/PacienteController.php
@@ -4,31 +4,34 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\PacienteRequest;
 use App\Models\Paciente;
-use App\Rules\CPFValidationRule;
 use Illuminate\Http\Request;
 
 class PacienteController extends Controller
 {
+    protected $request, $paciente;
+
+    public function __construct(Request $request, Paciente $paciente)
+    {
+        $this->request = $request;
+        $this->paciente = $paciente;
+    }
+
     /**
      * Display a listing of the resource.
      */
-    public function index(Request $request)
+    public function index()
     {
-        $request->validate([
-            'nome_completo' => 'string|max:45',
-            'cpf' => 'string|length:14',
-        ]);
 
-        if ($request->has('nome_completo')) {
-            $pacientes = Paciente::where('nome_completo', 'LIKE', "%{$request->nome_completo}%")
+        if ($this->request->has('nome_completo')) {
+            $pacientes = Paciente::where('nome_completo', 'LIKE', "%{$this->request->nome_completo}%")
                 ->get();
             if (!$pacientes) {
                 return response()->json(['message' => 'Nenhum paciente encontrado'], 404);
             }
         }
 
-        if ($request->has('cpf')) {
-            $pacientes = Paciente::where('cpf', 'LIKE', "%{$request->cpf}%")
+        if ($this->request->has('cpf')) {
+            $pacientes = Paciente::where('cpf', 'LIKE', "%{$this->request->cpf}%")
                 ->get();
             if (!$pacientes) {
                 return response()->json(['message' => 'Nenhum paciente encontrado'], 404);
@@ -49,10 +52,30 @@ class PacienteController extends Controller
      */
     public function store(PacienteRequest $request)
     {
-        $validateDate = $request->validate($request, [
-            "cpf" => [new CPFValidationRule],
-        ]);
-        dd($request->all());
+        $this->paciente->foto = $request->foto;
+        $this->paciente->nome_completo = $request->nome_completo;
+        $this->paciente->nome_mae = $request->nome_mae;
+        $this->paciente->data_nascimento = $request->data_nascimento;
+        $this->paciente->cpf = $request->cpf;
+        $this->paciente->cns = $request->cns;
+        $this->paciente->cep = $request->cep;
+        $this->paciente->logradouro = $request->logradouro;
+        $this->paciente->numero = $request->numero;
+        $this->paciente->complemento = $request->complemento;
+        $this->paciente->bairro = $request->bairro;
+        $this->paciente->cidade = $request->cidade;
+        $this->paciente->estado = $request->estado;
+
+        if ($this->paciente->save()) {
+            return response()->json([
+                'message' => 'Paciente cadastrado com sucesso'
+            ], 201);
+        }
+
+        return response()->json([
+            'message' => 'Erro ao cadastrar paciente',
+            'errors' => $this->paciente->errors()
+        ], 422);
     }
 
     /**
@@ -60,15 +83,31 @@ class PacienteController extends Controller
      */
     public function show(Paciente $paciente)
     {
-        //
+        return response()->json([
+            "paciente" => $paciente
+        ], 200);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Paciente $paciente)
+    public function update(Paciente $paciente)
     {
-        //
+        if ($paciente) {
+
+            $paciente->update($this->request->all());
+
+            return response()->json([
+                'message' => 'Paciente atualizado com sucesso',
+                'paciente' => $paciente
+            ], 204);
+        }
+
+        return response()->json([
+            'message' => 'Erro ao atualizar paciente',
+            'errors' => $paciente->errors()
+        ], 422);
+
     }
 
     /**
@@ -76,6 +115,15 @@ class PacienteController extends Controller
      */
     public function destroy(Paciente $paciente)
     {
-        //
+        if ($paciente) {
+            $paciente->delete();
+            return response()->json([
+                'message' => 'Paciente removido com sucesso'
+            ], 204);
+        }
+
+        return response()->json([
+            'message' => 'Erro ao remover paciente',
+        ], 422);
     }
 }
